939eebf42750d85db7531171afa5d406ac8fe5c1
Andy Taylor <Theador0691@gmail.com>
2015-11-01 16:27:55 +0000

    added the price

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019205634) do
+ActiveRecord::Schema.define(version: 20151101161635) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151019205634) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.float    "price"
   end
 
 end
