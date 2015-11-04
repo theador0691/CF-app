297e21cc129055bada468480f3645bcafc9c69a0
Andy Taylor <Theador0691@gmail.com>
2015-11-04 09:23:54 +0000

    Added a feature product and also started creating the orders controller and view as well as config the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'about'            => 'static_pages#about'
   get 'contact'          => 'static_pages#contact'
   root                      'static_pages#index'
-
+  resources :orders, only: [:index, :show, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
